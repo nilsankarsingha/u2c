c0baad3e8299fa2979e9282f32d5915b7d2b59b7
Nilsankar Singha <143016404+nilsankarsingha@users.noreply.github.com>
2023-08-25 01:53:04 +0530

    changes

--- src/Components/Web1/Home.jsx
+++ src/Components/Web1/Home.jsx
@@ -4,7 +4,14 @@ import './Stylesheet1.css'
 const Home = () => {
   return (
     <div className='container'>
-    <h2 className="text-center mt-3 text-danger">“After every dark night comes a glorious morning of a new day”</h2>
+    <h1 class="text-center mt-2">GOOD MORNING</h1>
+    <h2 class="text-center mt-2">WELCOME TO THE NEW DAY</h2>
+
+    <p class="text-end"><strong>From Nilsankar</strong></p>
+
+
+
+
     <div className="sun"></div>
     </div>
   )
